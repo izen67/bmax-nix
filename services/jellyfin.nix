a6ae283be8774f6d0ec33c58faa49b035b29fa76
izen67 <17254913+izen67@users.noreply.github.com>
2026-01-05 00:08:24 +0200

    Update jellyfin.nix

--- services/jellyfin.nix
+++ services/jellyfin.nix
@@ -18,4 +18,53 @@
     cacheDir  = "/var/cache/jellyfin";
     logDir    = "/var/log/jellyfin";
   };
+
+  # Create plexshare group
+  users.groups.plexshare = { };
+
+  # Samba login user
+  users.users.sambaplex = {
+    isNormalUser = true;
+    extraGroups = [ "plexshare" ];
+  };
+
+  # Admin access
+  users.users.globaladmin.extraGroups = [ "plexshare" ];
+
+  # Correct exFAT mount MUST set uid/gid or perms won't work
+  fileSystems."/media/plex" = {
+    device = "UUID=2A95-B9FC";
+    fsType = "exfat";
+    options = [
+      "uid=0"                # owner = root
+      "gid=plexshare"        # group owns the share
+      "fmask=0007"           # files = 770
+      "dmask=0007"           # dirs = 770
+      "noatime"
+      "nofail"
+    ];
+  };
+
+  # tmpfiles is meaningless for exFAT, but creating directory is fine
+  systemd.tmpfiles.rules = [
+    "d /media/plex 2770 root plexshare -"
+  ];
+
+  # Samba share
+  services.samba = {
+    enable = true;
+    securityType = "user";
+    shares = {
+      plex = {
+        comment = "Plex SSD";
+        path = "/media/plex";
+        browseable = true;
+        "read only" = false;
+        "guest ok" = false;
+        "create mask" = "0770";
+        "directory mask" = "0770";
+        "valid users" = [ "@plexshare" ];
+      };
+    };
+  };
 }
